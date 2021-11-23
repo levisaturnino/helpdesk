667ffdd59faf8be7cd1cf1ec9b6dcb7dc1a31e9d
Levi Saturnino <levisaturnino@gmail.com>
2021-11-23 01:53:15 -0300

    Criando a lista de tecnico

--- src/main/java/br/com/levisaturnino/helpdesk/controllers/TecnicoController.java
+++ src/main/java/br/com/levisaturnino/helpdesk/controllers/TecnicoController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping("/tecnicos")
 public class TecnicoController {
@@ -19,9 +22,14 @@ public class TecnicoController {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id){
-
         Tecnico obj = tecnicoService.findById(id);
-
         return ResponseEntity.ok().body(new TecnicoDTO(obj));
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<TecnicoDTO>> findByAll(){
+        List<Tecnico> list = tecnicoService.findByAll();
+        List<TecnicoDTO> listDTO = list.stream().map(TecnicoDTO::new).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 }
