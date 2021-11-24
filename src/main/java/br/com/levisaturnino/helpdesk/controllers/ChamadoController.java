be7b1463394be1cc192b54ab349928bd592d92cf
Levi Saturnino <levisaturnino@gmail.com>
2021-11-24 17:33:14 -0300

    Finalizado classe chamada.

--- src/main/java/br/com/levisaturnino/helpdesk/controllers/ChamadoController.java
+++ src/main/java/br/com/levisaturnino/helpdesk/controllers/ChamadoController.java
@@ -7,6 +7,10 @@ import br.com.levisaturnino.helpdesk.services.ChamadoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -30,4 +34,42 @@ public class ChamadoController {
         return ResponseEntity.ok().body(listDTO);
     }
 
+    @PostMapping
+    public ResponseEntity<ChamadoDTO> create(@RequestBody ChamadoDTO objDTO){
+        Chamado obj = chamadoService.create(objDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ChamadoDTO> update(@PathVariable Integer id,@Valid ChamadoDTO objDTO){
+        Chamado obj = chamadoService.update(id,objDTO);
+        return ResponseEntity.ok().body(new ChamadoDTO(obj));
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/br/com/levisaturnino/helpdesk/services/ChamadoService.java
+++ src/main/java/br/com/levisaturnino/helpdesk/services/ChamadoService.java
@@ -1,11 +1,21 @@
 package br.com.levisaturnino.helpdesk.services;
 
 import br.com.levisaturnino.helpdesk.domain.Chamado;
+import br.com.levisaturnino.helpdesk.domain.Cliente;
+import br.com.levisaturnino.helpdesk.domain.Tecnico;
+import br.com.levisaturnino.helpdesk.domain.dtos.ChamadoDTO;
+import br.com.levisaturnino.helpdesk.domain.enums.Prioridade;
+import br.com.levisaturnino.helpdesk.domain.enums.Status;
 import br.com.levisaturnino.helpdesk.repositories.ChamadoRepository;
 
+import br.com.levisaturnino.helpdesk.repositories.ClienteRepository;
+import br.com.levisaturnino.helpdesk.repositories.TecnicoRepository;
 import br.com.levisaturnino.helpdesk.services.exceptions.ObjectnotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.validation.Valid;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,6 +24,12 @@ public class ChamadoService {
 
     @Autowired
     private ChamadoRepository repository;
+
+    @Autowired
+    private TecnicoRepository tecnicoRepository;
+
+    @Autowired
+    private ClienteRepository clienteRepository;
 
     public Chamado findById(Integer id) {
         Optional<Chamado> obj = repository.findById(id);
@@ -24,4 +40,59 @@ public class ChamadoService {
         return repository.findAll();
     }
 
+    public Chamado create(ChamadoDTO objDTO) {
+        return repository.save(newChamado(objDTO));
+    }
+
+    public Chamado newChamado(ChamadoDTO objDTO) {
+        Optional<Tecnico> tecnico = tecnicoRepository.findById(objDTO.getTecnico());
+        Optional<Cliente> cliente = clienteRepository.findById(objDTO.getCliente());
+
+       tecnico.orElseThrow(() ->new ObjectnotFoundException("Técnico não encontrado! id: "+objDTO.getTecnico()));
+       cliente.orElseThrow(() ->new ObjectnotFoundException("Cliente não encontrado! id: "+objDTO.getCliente()));
+
+        Chamado chamado = new Chamado();
+
+        if(objDTO.getId() != null){
+            chamado.setId(objDTO.getId());
+        }
+
+        if(objDTO.getStatus().equals(2)){
+            chamado.setDataFechamento(LocalDate.now());
+        }
+
+        chamado.setTecnico(tecnico.get());
+        chamado.setCliente(cliente.get());
+        chamado.setPrioridade(Prioridade.toEnum(objDTO.getPrioridade()));
+        chamado.setStatus(Status.toEnum(objDTO.getStatus()));
+        chamado.setTitulo(objDTO.getTitulo());
+        chamado.setObservacoes(objDTO.getObservacoes());
+        return chamado;
+    }
+
+    public Chamado update(Integer id,@Valid ChamadoDTO objDTO) {
+        objDTO.setId(id);
+        Chamado oldObj = findById(id);
+        oldObj = newChamado(objDTO);
+        return repository.save(oldObj);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
